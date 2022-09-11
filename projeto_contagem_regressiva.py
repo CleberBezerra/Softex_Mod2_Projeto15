ac9879d648a9535c2f210568e4f3aba5cf2fa9a6
Cleber Bezerra <cleberbezerra666@gmail.com>
2022-09-11 12:23:17 -0300

    Adcionando melhorias no projeto

--- projeto_contagem_regressiva.py
+++ projeto_contagem_regressiva.py
@@ -1,10 +1,18 @@
 import time
+import os
+import winsound
+from turtle import clear
 
-print("Iniciando contagem regressiva: ")
-
+print("Iniciando CONTAGEM REGRESSIVA: ")
 for i in range(10, 0, -1):
-    time.sleep(2)
+    time.sleep(2) 
+    os.system("cls") #Limpa tela, apenas Windows
     print(i)
 
 if i == 1:
+    time.sleep(1)
     print("BOOOMMM!!!")
+    # Som de Beep
+    duration = 1500
+    freq = 440
+    winsound.Beep(freq, duration)
